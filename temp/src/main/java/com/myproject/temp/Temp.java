056f86510d48d3b2399d869b3a99620334f6e3b6
Arusmita12 <100747869+Arusmita12@users.noreply.github.com>
2023-09-09 15:14:03 +0530

    Add files via upload

---
+++ temp/src/main/java/com/myproject/temp/Temp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myproject.temp;
+
+/**
+ *
+ * @author user
+ */
+public class Temp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
